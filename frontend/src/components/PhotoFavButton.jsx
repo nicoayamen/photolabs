2c702be9ba768b01ab0b7cf28178178caa6a1afe
nicoayamen <nicolas.ayala4@gmail.com>
2024-05-22 19:25:34 -0600

    add onclick and useState for user click on heart

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,11 +3,17 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+function PhotoFavButton(props) {
+  const [like, SetLike] = useState(false);
+
+  const handleClick = () => {
+    SetLike(prevLike => !prevLike);
+  };
+
   return (
-    <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+    <div className={"photo-list__fav-icon"} onClick={handleClick}>
+      <div className={"photo-list__fav-icon-svg"}>
+        <FavIcon selected={like} />
       </div>
     </div>
   );
