ee8d76d572422bd3fb8998f9c9ddcfb6dc202f42
nicoayamen <nicolas.ayala4@gmail.com>
2024-05-22 19:02:49 -0600

    use BEM naming for jsx

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,15 +4,15 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = ({photo}) => {
   return (
-    <li className="photo-list" key={photo.id}>
-    <div className="photo-list__item">
+    <li className="photo-list__item" key={photo.id}>
+    <div className="photo-list__item__details">
       <img className="photo-list__image" src={photo.imageSource}></img>
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={photo.profile}></img>
         <div className="photo-list__user-info">
           <span>{photo.username}</span>
           <div className="photo-list__user-location">
-            <span>{photo.location.city}</span>
+            <span>{photo.location.city}, </span>
             <span>{photo.location.country}</span>
           </div>
         </div>
