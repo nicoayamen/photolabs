bfcdb1ff4ad9aa94b256988b89698ed08a9550b8
nicoayamen <nicolas.ayala4@gmail.com>
2024-05-23 19:05:21 -0600

    remove sample for mockdata

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -3,24 +3,6 @@ import TopicListItem from "./TopicListItem";
 
 import "../styles/TopicList.scss";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
-
 const TopicList = (props) => {
   
   const parsedTopic = props.topics.map((topic) =>
