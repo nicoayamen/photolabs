1a7d12e5baef5d65e026ec994d772c7fc6999489
nicoayamen <nicolas.ayala4@gmail.com>
2024-05-23 18:40:23 -0600

    refactor code to take from HomeRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,16 @@
 import React from 'react';
 
-// import PhotoListItem from './components/PhotoListItem';
-import PhotoList from 'components/PhotoList';
 import './App.scss';
-
+import HomeRoute from 'routes/HomeRoute';
+import topics from 'mocks/topics';
+import photos from 'mocks/photos';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  const photos = Array.from({ length: 3 }, (_, index) => index + 1);
-
   return (
     <div className="App">
-      {/* {photos.map((_, index) => (
-        <PhotoListItem
-          key={index}
-          photo={sampleDataForPhotoListItem}
-        />
-      ))} */}
-      <PhotoList />
-    </div>
+    <HomeRoute topics={topics} photos={photos}/>
+  </div>
   );
 };
 
