55940381e6e7ba5598a486992b4ee1b4bb31ef94
nicoayamen <nicolas.ayala4@gmail.com>
2024-05-23 19:51:08 -0600

    set state for modal open

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 import topics from 'mocks/topics';
 import photos from 'mocks/photos';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="App">
-    <HomeRoute topics={topics} photos={photos}/>
+    <HomeRoute topics={topics} 
+    photos={photos}
+    open={open}
+    setOpen={setOpen}/>
+    {open && <PhotoDetailsModal />}
   </div>
   );
 };
