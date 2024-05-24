f03b99bdf4c88a682052de91c684ed1ba2b149cd
nicoayamen <nicolas.ayala4@gmail.com>
2024-05-23 19:02:16 -0600

    add homeroute to show homepage

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../styles/HomeRoute.scss';
+import TopNavigation from 'components/TopNavigationBar';
+import PhotoList from 'components/PhotoList';
 
-const HomeRoute = () => {
+//Homepage route to view photos and nav bar from homepage
+const HomeRoute = (props) => {
+
+  //Set inital state of liked photos to empty array
+  const [likedPhotos, setLikedPhotos] = useState([]);
+
+  //Function to check if photoID is already present in likedPhotos state, if not, add the photoID
+  const toggleLike = function(photoId) {
+    if (likedPhotos.includes(photoId)) {
+      const updatedLikedPhotos = likedPhotos.filter(id => id !== photoId);
+      setLikedPhotos(updatedLikedPhotos);
+    } else {
+      const updatedLikedPhotos = [...likedPhotos, photoId];
+      setLikedPhotos(updatedLikedPhotos);
+    }
+  };
+
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigation topics={props.topics} likedPhotos={likedPhotos} />
+      <PhotoList photos={props.photos} likedPhotos={likedPhotos} toggleLike={toggleLike} />
     </div>
   );
 };
