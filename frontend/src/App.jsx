b7635ca25bcffddff69c45ba4cf5a3c0e0682a62
nicoayamen <nicolas.ayala4@gmail.com>
2024-05-26 13:36:05 -0600

    reformat and clean up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
 import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 import useApplicationData from 'hooks/useApplicationData';
-
+// Note: Rendering a single component to build components in isolation
 const App = () => {
   const { state, toggleModal, toggleLike  } = useApplicationData();
 
